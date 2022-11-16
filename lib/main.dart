d6af1e4b4a1955ef6d977fb2b5e974a1033663c2
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-16 18:18:12 -0500

    Basic Config web app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:actualizamos_web_app/src/my_web_page.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() {
+  setPathUrlStrategy();
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        title: 'Basic Landing webpage',
+        home: MyWebPage(),
+      ),
+    ),
+  );
+}
