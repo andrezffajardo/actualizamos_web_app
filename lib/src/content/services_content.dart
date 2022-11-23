ebd92d3384051995b5619ca12af14a2c4982b8de
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-22 23:02:32 -0500

    Add change card elevation state

--- lib/src/content/services_content.dart
+++ lib/src/content/services_content.dart
@@ -1,11 +1,15 @@
 import 'package:carousel_slider/carousel_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ServicesContent extends StatelessWidget {
+class ServicesContent extends HookConsumerWidget {
   const ServicesContent({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cardElevation = useState(5.0);
+
     return Container(
         height: 1000,
         color: Colors.white,
@@ -13,35 +17,43 @@ class ServicesContent extends StatelessWidget {
           children: [
             Column(
               children: [
-                Card(
-                  elevation: 20,
-                  child: Container(
-                    height: 400,
-                    width: 350,
-                    //width: double.infinity,
-                    decoration: BoxDecoration(
-                        //borderRadius: BorderRadius.circular(15.0),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('images/servicios4.jpg'))),
+                MouseRegion(
+                  onEnter: (value) {
+                    cardElevation.value = 20.0;
+                  },
+                  onExit: (value) {
+                    cardElevation.value = 5.0;
+                  },
+                  child: Card(
+                    elevation: cardElevation.value,
+                    child: Container(
+                      height: 400,
+                      width: 350,
+                      //width: double.infinity,
+                      decoration: BoxDecoration(
+                          //borderRadius: BorderRadius.circular(15.0),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('images/servicios4.jpg'))),
 
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 25.0,
-                        right: 25.0,
-                        top: 300,
-                      ),
-                      child: Text(
-                        'NORMAS INTERNACIONALES DE INFORMACION FINANCIERA - NIIF',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF0000ff),
-                          fontWeight: FontWeight.bold,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 25.0,
+                          right: 25.0,
+                          top: 300,
+                        ),
+                        child: Text(
+                          'NORMAS INTERNACIONALES DE INFORMACION FINANCIERA - NIIF',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF0000ff),
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
+                    margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0),
                   ),
-                  margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0),
                 ),
                 Card(
                   child: Container(
