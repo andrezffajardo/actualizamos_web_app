b6da1101566228aa0e6a5d405a40144fc301b99d
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-29 18:25:29 -0500

    MobileExpContent styling

--- lib/src/content/exp_content.dart
+++ lib/src/content/exp_content.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
-
 import '../widget/contact_button.dart';
+import '../widget/responsive_widget.dart';
 
-class ExpContent extends StatelessWidget {
+class ExpContent extends ResponsiveWidget {
   const ExpContent({super.key});
+
+  @override
+  Widget buildDesktop(BuildContext context) {
+    return DesktopExpContent();
+  }
+
+  @override
+  Widget buildMobile(BuildContext context) {
+    return MobileExpContent();
+  }
+}
+
+class DesktopExpContent extends StatelessWidget {
+  const DesktopExpContent({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +29,6 @@ class ExpContent extends StatelessWidget {
               flex: 2,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                //crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Image.asset(
                     'images/img-asesoria2.png',
@@ -25,9 +38,6 @@ class ExpContent extends StatelessWidget {
                 ],
               ),
             ),
-            // Expanded(
-            //   child: SizedBox.shrink(),
-            // ),
             Expanded(
               flex: 1,
               child: Column(
@@ -89,5 +99,56 @@ class ExpContent extends StatelessWidget {
             //Expanded(child: SizedBox.shrink()),
           ],
         ));
+  }
+}
+
+class MobileExpContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/img-asesoria2.png',
+              height: 300,
+            ),
+            SizedBox(height: 25),
+            Text(
+              'CRECIENDO CON NUESTROS CLIENTES',
+              style: TextStyle(
+                fontSize: 18,
+                color: Color(0xFF414141),
+              ),
+            ),
+            Text(
+              'Más de 20 años\n de experiencia',
+              style: TextStyle(
+                fontSize: 50,
+                color: Color(0xFF0000ff),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 25),
+            SizedBox(
+              height: 100,
+              width: 350,
+              child: Text(
+                'Conocemos muy bien las necesidades de las compañías del sector real, las instituciones publicas, las organizaciones sin fines de lucro y los pequeños negocios con rápido crecimiento.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF414141),
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            ContactButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
