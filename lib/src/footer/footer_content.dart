079b10bc391f42f148f5b4a9b624f7dc3e11ece6
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-30 21:09:08 -0500

    MobileFooterContent styling

--- lib/src/footer/footer_content.dart
+++ lib/src/footer/footer_content.dart
@@ -4,8 +4,25 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:actualizamos_web_app/src/my_web_page.dart';
 import 'package:actualizamos_web_app/src/footer/footer_content_button.dart';
 
-class FooterContent extends HookConsumerWidget {
+import '../widget/contact_button.dart';
+import '../widget/responsive_widget.dart';
+
+class FooterContent extends ResponsiveWidget {
   const FooterContent({super.key});
+
+  @override
+  Widget buildDesktop(BuildContext context) {
+    return DesktopFooterContent();
+  }
+
+  @override
+  Widget buildMobile(BuildContext context) {
+    return MobileFooterContent();
+  }
+}
+
+class DesktopFooterContent extends HookConsumerWidget {
+  const DesktopFooterContent({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -389,6 +406,299 @@ class FooterContent extends HookConsumerWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MobileFooterContent extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      color: Colors.grey.shade200,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('images/logo.png', height: 80.0),
+            SizedBox(height: 25),
+            Text(
+              'Navega',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color(0xFF0000ff),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            FooterContentButton(
+                onTap: () =>
+                    ref.read(currentPageProvider.state).state = sliderKey,
+                text: 'Home'),
+            FooterContentButton(
+                onTap: () =>
+                    ref.read(currentPageProvider.state).state = aboutKey,
+                text: 'Nosotros'),
+            FooterContentButton(
+                onTap: () =>
+                    ref.read(currentPageProvider.state).state = servicesKey,
+                text: 'Servicios'),
+            FooterContentButton(
+                onTap: () =>
+                    ref.read(currentPageProvider.state).state = contactKey,
+                text: 'Contáctenos'),
+            SizedBox(height: 25),
+            Text(
+              'Servicios',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color(0xFF0000ff),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.anglesRight,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Consultoría Gerencial',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.anglesRight,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Auditoría y Revisoría Fiscal',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.anglesRight,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Outsourcing Contable',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.anglesRight,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Impuestos',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.anglesRight,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'NIIF',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Contacto',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color(0xFF0000ff),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 60,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.locationDot,
+                  ),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: 220,
+                    height: 250,
+                    child: Text(
+                      'Av. 2 Nte. # 7N-55 Of. 319,\nCali, Valle del Cauca,\nColombia.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF414141),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.phone,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '+57 2 653 48 68',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Row(
+                children: [
+                  Icon(
+                    color: Color(0xFF0000ff),
+                    size: 18.0,
+                    FontAwesomeIcons.envelope,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'info@actualizamos.com.co',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF414141),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: FaIcon(FontAwesomeIcons.facebookF),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.grey.shade500),
+                  shape: MaterialStatePropertyAll(CircleBorder()),
+                  padding: MaterialStatePropertyAll(EdgeInsets.all(15.0)),
+                  elevation: MaterialStatePropertyAll(5.0),
+                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.hovered))
+                        return Color(0xFFced842); //<-- SEE HERE
+                      return null; // Defer to the widget's default.
+                    },
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: FaIcon(FontAwesomeIcons.instagram),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.grey.shade500),
+                  shape: MaterialStatePropertyAll(CircleBorder()),
+                  padding: MaterialStatePropertyAll(EdgeInsets.all(15.0)),
+                  elevation: MaterialStatePropertyAll(5.0),
+                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.hovered))
+                        return Color(0xFFced842); //<-- SEE HERE
+                      return null; // Defer to the widget's default.
+                    },
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: FaIcon(FontAwesomeIcons.twitter),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.grey.shade500),
+                  shape: MaterialStatePropertyAll(CircleBorder()),
+                  padding: MaterialStatePropertyAll(EdgeInsets.all(15.0)),
+                  elevation: MaterialStatePropertyAll(5.0),
+                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.hovered))
+                        return Color(0xFFced842);
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+            ]),
+          ],
+        ),
       ),
     );
   }
